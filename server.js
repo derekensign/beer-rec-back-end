5dc96657d7a46a56093c1c8c5c186507f576b71c
Derek Ensign <derekensign@gmail.com>
2021-04-12 16:20:41 -0500

    fixed PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(require('cors')())
 
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`port running on ${PORT}`)
   routesReport.print()
