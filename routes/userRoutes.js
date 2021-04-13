77e6a6a629416b0830e1550053e32a6b12b1f995
Derek Ensign <derekensign@gmail.com>
2021-04-12 22:55:14 -0500

    added login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ const userRoutes = express.Router()
 const axios = require('axios')
 
 userRoutes.post('/', userController.createUser)
-// userRoutes.post('/login', userController.login)
+userRoutes.post('/login', userController.login)
 
 module.exports = userRoutes
